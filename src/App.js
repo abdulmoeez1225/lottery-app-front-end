912452ef681cb623d37b9e9821f4011ce6820cf7
abdulmoeez1225 <abdulmoeez1225@gmail.com>
2022-06-08 00:13:08 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,156 @@
-import logo from './logo.svg';
-import './App.css';
+import { Card, Col, Row } from "antd";
+import logo from "./logo.svg";
+import { useEffect, useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+import CardHeader from "@mui/material/CardHeader";
+
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import "./App.css";
+
+import {
+  connectMetaMaskaction,
+  EnterLottery,
+  getBalance,
+  LotteryContract,
+} from "./component/wallet";
+import { TextField } from "@mui/material";
+import { ethers, providers } from "ethers";
 
 function App() {
+  const [isWalletconnected, setisWalletConnected] = useState(false);
+  const [IntractWithContract, setIntractWithContract] = useState();
+  const [allPlayer, setAllPlayer] = useState([]);
+  const [balance, setBalance] = useState(0);
+  const [enterdValue, setEnterdValue] = useState();
+
+  const [manager, setManager] = useState("");
+  useEffect(() => {
+    if (isWalletconnected) {
+      setInterval(() => {
+        ReadyToIntractWithContract();
+      }, 2000);
+    }
+  }, [isWalletconnected]);
+
+  const ReadyToIntractWithContract = async () => {
+    if (isWalletconnected) {
+      const intractWithWeb3 = await LotteryContract();
+      setIntractWithContract(intractWithWeb3);
+      const managerResult = await intractWithWeb3.manager();
+      const playerResult = await intractWithWeb3.getAllPlayers();
+      // const balance = await window.web3.eth.getBalance(managerResult);
+
+      const balanceResult = await getBalance();
+
+      setBalance(balanceResult);
+      setManager(managerResult);
+
+      setAllPlayer(playerResult);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            {/* <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton> */}
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            ></Typography>
+            <Button
+              variant="contained"
+              color="success"
+              onClick={async () => {
+                const result = await connectMetaMaskaction();
+                setTimeout(() => {
+                  if (result) {
+                    setisWalletConnected(true);
+                  } else {
+                    setisWalletConnected(false);
+                  }
+                }, 1000);
+              }}
+            >
+              Connect
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Grid container xs={12} justifyContent={"center"} alignContent={"center"}>
+        <Grid
+          container
+          xs={6}
+          justifyContent={"center"}
+          alignContent={"center"}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Typography variant="subtitle1" component="div" sx={{ flexGrow: 1 }}>
+            This Contract is managed by {manager}
+          </Typography>
+          <Typography variant="subtitle1" component="div" sx={{ flexGrow: 1 }}>
+            Total Number of Player {allPlayer.length}
+          </Typography>
+
+          <Card
+            style={{
+              borderRadius: 20,
+              width: "100%",
+              backgroundColor: "rgba(156, 161, 157,0.3)",
+              border: "1px solid black",
+              marginTop: 20,
+            }}
+          >
+            <CardHeader
+              title={"Try Your Luck"}
+              style={{ textAlign: "center" }}
+            />
+            <CardContent>
+              <TextField
+                id="outlined-basic"
+                label="Enter Value"
+                onChange={(e) => {
+                  setEnterdValue(e.target.value);
+                }}
+                style={{ width: "100%" }}
+                type="number"
+                variant="outlined"
+              />
+            </CardContent>
+            <CardActions style={{ justifyContent: "center" }}>
+              <Button
+                size="small"
+                variant="contained"
+                onClick={() => {
+                  EnterLottery(enterdValue);
+                }}
+              >
+                Submit
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
---
+++ src/component/wallet.js
@@ -0,0 +1,153 @@
+// import detectEthereumProvider from "@metamask/detect-provider";
+import { Contract, ethers } from "ethers";
+// import Web3 from "web3";
+
+const contractAddress = "0x6e894E7633F8d9faaC4CE6834640fdbeAB3A746f";
+
+const minAbi = [
+  { inputs: [], stateMutability: "nonpayable", type: "constructor" },
+  {
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getAllPlayers",
+    outputs: [{ internalType: "address[]", name: "", type: "address[]" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "manager",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "random",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+export const connectMetaMaskaction = async () => {
+  // Check metamask is install or not
+  if (window.ethereum) {
+    // await window.ethereum.enable();
+    await window.ethereum
+      .request({
+        method: "eth_requestAccounts",
+      })
+      .then((res) => {
+        console.log(res);
+        return res;
+      })
+      .catch((err) => {
+        console.log(err);
+        return err;
+      });
+
+    // window.web3 = new Web3(window.ethereum);
+
+    // const provider = await detectEthereumProvider();
+    // if (provider !== window.ethereum) {
+    //   // console.log("top provider");
+    //   // @ts-ignore
+    //   window.web3 = new Web3(window.ethereum);
+    // } else {
+    // console.log("bottom provider");
+    // @ts-ignore
+
+    //window.open("https://support.wwf.org.uk", "_blank");
+    // }
+    window.ethereum &&
+      window.ethereum.request({ method: "eth_chainId" }).then((chainId) => {});
+    window.ethereum &&
+      window.ethereum.request({ method: "eth_accounts" }).then((accounts) => {
+        const chainId = window.ethereum.chainId;
+
+        // if (window.ethereum) {
+
+        // }
+      });
+
+    window.ethereum &&
+      window.ethereum.on("accountsChanged", async (accounts) => {
+        // console.log(accounts);
+      });
+    window.ethereum &&
+      window.ethereum.on("chainChanged", async (chainId) => {
+        // console.log("chainChanged............");
+      });
+
+    const metamaskResult = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    // .then(async () => {
+    //   const chainId = window.ethereum.chainId;
+    //   const accounts = await window.web3.eth.getAccounts();
+    //   const balance = await window.web3.eth.getBalance(accounts[0]);
+    //   return true;
+    // })
+    // .catch((error) => {
+    //   // console.log("error::::", error);
+    //   return false;
+    // });
+    if (metamaskResult) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+export const LotteryContract = async () => {
+  // const data = await window.web3.eth.Contract(minAbi, contractAddress);
+  let provider = new ethers.providers.Web3Provider(window.ethereum);
+  // const provider = new ethers.providers.JsonRpcProvider(rpc);
+
+  const signer = provider.getSigner();
+  return await new Contract(contractAddress, minAbi, signer);
+};
+
+export const getBalance = async () => {
+  const accounts = await window.ethereum.request({
+    method: "eth_requestAccounts",
+  });
+
+  let provider = new ethers.providers.Web3Provider(window.ethereum);
+
+  let intractWithWeb3 = await LotteryContract();
+  const managerAddress = await intractWithWeb3.manager();
+  let balance = await provider.getBalance(managerAddress);
+  return await ethers.utils.formatEther(balance);
+};
+
+export const EnterLottery = async (value) => {
+  let provider = new ethers.providers.Web3Provider(window.ethereum);
+  let intractWithWeb3 = await LotteryContract();
+  const sendAmount = await intractWithWeb3.enter({
+    value: ethers.utils.parseEther(value),
+  });
+  return sendAmount;
+};
